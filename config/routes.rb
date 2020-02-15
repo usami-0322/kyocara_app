c6cbd4a0c4c9e720e8ef2e2c02d56e59316e8038
usami-0322 <usaco0322@gmail.com>
2020-02-15 02:53:13 +0000

    投稿画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
+  get "posts/new" => "posts#new"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  post "posts/create" => "posts#create"
+  
   get "/" => "home#top"
-  get "/about" => "home#about"
+  get "about" => "home#about"
 end
